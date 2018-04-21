837720c9dc5cf62e9c5933695babc56e3f094f70
jeffliang0318 <liangnash0318@gmail.com>
2018-04-21 01:05:57 -0700

    add passport and google strategy

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
