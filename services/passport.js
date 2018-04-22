6b06e335169f43a78a1b51cbd9ef6065824fae24
jeffliang0318 <liangnash0318@gmail.com>
2018-04-21 23:26:06 -0700

    tweak google strategy for proxy true

--- services/passport.js
+++ services/passport.js
@@ -27,7 +27,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     // callback
     (accessToken, refreshToken, profile, done) => {
