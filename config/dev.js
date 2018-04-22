1b573412a015b6fe121a241f7649b42f53b8267f
jeffliang0318 <liangnash0318@gmail.com>
2018-04-21 22:49:30 -0700

    update config file

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '256843099177-meu784dckg8cjcbrjsokm24opklvq0o1.apps.googleusercontent.com',
+  googleClientSecret: 'wkVKYLqZpTV300aeMQ3khAfr',
+  mongoURI: 'mongodb://Jeff:password@ds153869.mlab.com:53869/food-filter',
+  cookieKey: 'kjsanflksjflwqbedjlcisaunfrl'
+};
