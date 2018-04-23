1a62f4a9e74f0a58ab42d7ca1fd53c0ea99e9e5c
jeffliang0318 <liangnash0318@gmail.com>
2018-04-23 10:39:15 -0700

    add fetchUser

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,10 @@
-export default function(state = {}, action) {
-  console.log(action.type);
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
+  console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload  || false;
     default:
     return state;
   }
