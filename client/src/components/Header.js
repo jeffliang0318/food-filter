1e1c4d1dbe06fad55ea9edfca18df503ac001de5
jeffliang0318 <liangnash0318@gmail.com>
2018-04-23 01:28:55 -0700

    423 work, fetch_user test

---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends  Component {
+  render() {
+    return (
+      <nav>
+        <div>
+          <a href="#">FoodFilter</a>
+          <a href="#">Login With Google</a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,7 @@
+export default function(state = {}, action) {
+  console.log(action.type);
+  switch (action.type) {
+    default:
+    return state;
+  }
+}
