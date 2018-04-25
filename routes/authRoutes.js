4d2ece37b815d55225b1706afa7ad5376af38041
jiangtt18 <tjiang90111@gmail.com>
2018-04-25 12:02:18 -0700

    add

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,6 @@
 const passport = require('passport');
-const express = require('express');
-// const localStrategy = require('passport-local').Strategy;
-
-//START REGULAR ROUTES
-const mongoose = require('mongoose');
-const User = mongoose.model('users'); // Is this the right the way to get User class from model?
-//users refer to the collection in the mongoDB database; .model makes a copy of the schema
 
 module.exports = app => {
-
   app.get(
     '/auth/google',
     passport.authenticate('google', {
@@ -30,11 +22,8 @@ module.exports = app => {
   });
 
   app.post('/api/current_user', async (req, res) => {
-    // console.log(req);
-    // console.log(res);
     const body = req.body;
     // const body = req.res;
-    console.log(body);
     const  ingredient  = req.body;
     req.user.allergyIngredient = ingredient;
     const user = await req.user.save();
@@ -44,9 +33,7 @@ module.exports = app => {
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    req.flash('success_msg', 'You are logged out');
+    // redirect to index after logout
     res.redirect('/');
   });
-
-
-}
+};
