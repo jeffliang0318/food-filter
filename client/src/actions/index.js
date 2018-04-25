473fa7efa2bdc4ed5edb81e2d88052638c7802f0
jeffliang0318 <liangnash0318@gmail.com>
2018-04-25 10:23:49 -0700

    clear non-nesscery comment

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,12 +7,6 @@ export const fetchUser = () => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-// export const handleToken = token => async dispatch => {
-//   const res = await axios.post('/api/stripe', token);
-//
-//   dispatch({ type: FETCH_USER, payload: res.data });
-// };
-
 export const updateAllergyIngredient = (ingredient) => async dispatch => {
 
   const res = await axios.post('/api/current_user', ingredient);
