18bc2808577a486a9ef8e4cdea6a0a829c75fa1e
jiangtt18 <tjiang90111@gmail.com>
2018-04-25 21:45:17 -0700

    pull from master

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const exphbs = require('express-handlebars');
-const expressValidator = require('express-validator');
-const flash = require('connect-flash');
-const session = require('express-session');
-const LocalStrategy = require('passport-local').Strategy;
-const mongo = require('mongodb');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
@@ -15,17 +7,8 @@ const keys = require('./config/keys');
 require('./models/User');
 //need to load User before passport otherwise Schema couldnt been found
 require('./services/passport');
+
 const app = express();
-//
-//
-// var routes = require('./routes/index');
-// var users = require('./routes/users');
-
-
-// BodyParser middleware parse incoming requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use(bodyParser.json());
 app.use(
@@ -35,60 +18,13 @@ app.use(
     keys: [keys.cookieKey]
   })
 );
-// Express session
-// app.use(session({
-//   secret: 'secret',
-//   saveUninitialized: true,
-//   resave: true
-// }));
-
-// Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-// app.set('views', path.join(__dirname, 'views'));
-// app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-// app.set('view engine', 'handlebars');
-
-
 mongoose.connect(keys.mongoURI);
+
 require('./routes/authRoutes')(app);
 
-// Express Validator
-// app.use(expressValidator({
-//   errorFormatter: function(param, msg, value) {
-//       var namespace = param.split('.')
-//       , root    = namespace.shift()
-//       , formParam = root;
-//
-//     while(namespace.length) {
-//       formParam += '[' + namespace.shift() + ']';
-//     }
-//     return {
-//       param : formParam,
-//       msg   : msg,
-//       value : value
-//     };
-//   }
-// }));
-
-// Connect Flash
-// app.use(flash());
-//
-// Global Vars for Flash messages
-// app.use(function (req, res, next) {
-//   res.locals.success_msg = req.flash('success_msg');
-//   res.locals.error_msg = req.flash('error_msg');
-//   res.locals.error = req.flash('error'); // for passport sets its own error message on error
-//   res.locals.user = req.user || null;
-//   next();
-// });
-
-
-// app.use('/', routes);
-// app.use('/users', users);
-// //
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT);
 
--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,9 @@
-// // const keys = require('../config/keys');
-// // const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// // const passport = require('passport');
-// // const mongoose = require('mongoose');
-// // const User = mongoose.model('users');
-//
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const PassportLocalStrategy = require('passport-local');
-//
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-// const User = mongoose.model('users');
+const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
   // user.id is user id not googleId, cause user might sign in in different strategy
@@ -46,25 +38,3 @@ passport.use(
     }
   )
 );
-
-// //
-// // // ADDING REGULAR LOGIN
-// //
-// // passport.use(new PassportLocalStrategy(
-// //   function (username, password, done) {
-// //     User.getUserByUsername(username, function (err, user) {
-// //       if (err) throw err;
-// //       if (!user) {
-// //         return done(null, false, { message: 'Unknown User' });
-// //       }
-// //
-// //       User.comparePassword(password, user.password, function (err, isMatch) {
-// //         if (err) throw err;
-// //         if (isMatch) {
-// //           return done(null, user);
-// //         } else {
-// //           return done(null, false, { message: 'Invalid password' });
-// //         }
-// //       });
-// //     });
-// //   }));
