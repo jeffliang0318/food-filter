44316caf680a88631f128bc7465858ffee6cb986
jeffliang0318 <liangnash0318@gmail.com>
2018-04-26 22:40:33 -0700

    ready for heroku againgit add .

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -20,7 +20,7 @@ module.exports = app => {
     passport.authenticate('google'),
     (req, res) => {
       // redirect to index after google auth
-      res.redirect('/')
+      res.redirect('/user')
     }
   );
 
