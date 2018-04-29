a4c6032d7440e15580f68558ac34fffb7d908750
Ziyan Wang <zw301@nyu.edu>
2018-04-28 20:28:56 -0700

    Search image load page styling

--- client/src/components/search_upload_upc/search_upload_upc.jsx
+++ client/src/components/search_upload_upc/search_upload_upc.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 class searchUploadUPC extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {upc: ""};
-        this.handleClick = this.handleClick.bind(this);
+      super(props);
+      this.state = {upc: ""};
+      this.handleClick = this.handleClick.bind(this);
+      this.handleExampleClick = this.handleExampleClick.bind(this);
     }
 
     handleClick(e) {
-        this.props.fetch_product(window.upcCode)
-        .then(() => this.props.history.push("/results"));
+      this.props.fetch_product(window.upcCode)
+      .then(() => this.props.history.push("/results"));
+    }
 
+    handleExampleClick(e) {
+      // alert(e.currentTarget.id);
+      this.props.fetch_product(e.currentTarget.id)
+      .then(() => this.props.history.push("/results"));
     }
 
     render() {
@@ -39,10 +45,71 @@ class searchUploadUPC extends React.Component {
                 </div>
             </div>
             <div id="result_strip">
-                <ul className="thumbnails">Please Wait for image load...</ul>
+                <p>Barcode Reading Area</p>
+                <ul className="thumbnails">
+                  <li>
+                    <div class="thumbnail">
+                      <div class="imgWrapper">
+                        <img src="http://res.cloudinary.com/dwanjkcku/image/upload/c_scale,w_500/v1524763023/barcode.png" alt="barcodeDemo" />
+                      </div>
+                      <div class="caption">
+                        <h4 class="code"></h4>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
             </div>
-            <div>
-            <button onClick={this.handleClick}>Search Item</button>
+            <div className="searchButton-container">
+              <button className="searchButton" onClick={this.handleClick}>Search</button>
+            </div>
+            <div className="recentlySearch">
+              <h4>Recently Search</h4>
+              <ul>
+                <li className="searchExample-li" id="222222222222" onClick={this.handleExampleClick}>
+                  <div>
+                    <div class="searchExample-imgWrapper">
+                      <img src="http://res.cloudinary.com/dwanjkcku/image/upload/c_scale,w_500/v1524763023/barcode.png" alt="barcodeDemo" />
+                    </div>
+                    <div class="searchExample-codeContainer">
+                      <p class="searchExample-code">123456789123</p>
+                    </div>
+                  </div>
+                </li>
+
+                <li className="searchExample-li">
+                  <div>
+                    <div class="searchExample-imgWrapper">
+                      <img src="http://res.cloudinary.com/dwanjkcku/image/upload/c_scale,w_500/v1524763023/barcode.png" alt="barcodeDemo" />
+                    </div>
+                    <div class="searchExample-codeContainer">
+                      <p class="searchExampleCode">123456789123</p>
+                    </div>
+                  </div>
+                </li>
+
+                <li className="searchExample-li">
+                  <div>
+                    <div class="searchExample-imgWrapper">
+                      <img src="http://res.cloudinary.com/dwanjkcku/image/upload/c_scale,w_500/v1524763023/barcode.png" alt="barcodeDemo" />
+                    </div>
+                    <div class="searchExample-codeContainer">
+                      <p class="searchExampleCode">123456789123</p>
+                    </div>
+                  </div>
+                </li>
+
+                <li className="searchExample-li last-li">
+                  <div>
+                    <div class="searchExample-imgWrapper">
+                      <img src="http://res.cloudinary.com/dwanjkcku/image/upload/c_scale,w_500/v1524763023/barcode.png" alt="barcodeDemo" />
+                    </div>
+                    <div class="searchExample-codeContainer">
+                      <p class="searchExampleCode">123456789123</p>
+                    </div>
+                  </div>
+                </li>
+
+              </ul>
             </div>
           </div>
         );
