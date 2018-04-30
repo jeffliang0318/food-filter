5abb594df9c96a2a5db662b2a880d8b12bc1fc8b
jiangtt18 <tjiang90111@gmail.com>
2018-04-29 20:11:56 -0700

    refactory validationform

--- routes/users.js
+++ routes/users.js
@@ -9,31 +9,9 @@ module.exports = app => {
   app.post('/users/register', function (req, res) {
     //
     const {email,username,name,password,password2} = req.body;
-    // // _validateFrom(req);
-    //
-    // req.checkBody('email', 'Email is required').notEmpty();
-  	// req.checkBody('email', 'Email is not valid').isEmail();
-  	// req.checkBody('username', 'Username is required').notEmpty();
-  	// req.checkBody('name', 'Name is required').notEmpty();
-  	// req.checkBody('password', 'Password is required').notEmpty();
-  	// req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
-    //
-  	// var errors = req._validationErrors();
-  //
-  //   var email = req.body.email;
-	// var username = req.body.username;
-	// var name = req.body.name;
-	// var password = req.body.password;
-	// var password2 = req.body.password2;
-	// Validation
-	req.checkBody('email', 'Email is required').notEmpty();
-	req.checkBody('email', 'Email is not valid').isEmail();
-	req.checkBody('username', 'Username is required').notEmpty();
-	req.checkBody('name', 'Name is required').notEmpty();
-	req.checkBody('password', 'Password is required').notEmpty();
-	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    _validateFrom(req);
 
-	var errors = req.validationErrors();
+	   var errors = req.validationErrors();
 
 
   	if (errors) {
